2cb9a4ab651a27011f0311e1b457519dfe05b478
Tomasz Magulski <tomasz@magullab.io>
2017-08-15 18:25:33 +0800

    JSON transformation

---
+++ main.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+
+def main():
+    with open('in.json') as fin:
+        data = json.loads(fin.read().decode("utf-8-sig").encode("utf-8"))
+
+    for feature in data['features']:
+        feature['properties']['comments'] = []
+        feature['properties']['responses'] = [
+            {
+                'input': '1-tytu',
+                'value': feature['properties']['1-tytu']
+            },
+            {
+                'input': '2-opis',
+                'value': feature['properties']['2-opis']
+            },
+            {
+                'input': '3-symbol',
+                'value': [feature['properties']['3-symbol']]
+            },
+        ]
+        del feature['properties']['1-tytu']
+        del feature['properties']['2-opis']
+        del feature['properties']['3-symbol']
+
+    with open('out.json', 'w') as fout:
+        json.dump(data, fout)
+
+
+if __name__ == '__main__':
+    main()
